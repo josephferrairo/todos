33ef1cd63cbf3be7228074a271e6b9a5096b83ea
josephferrairo <josephferrairo@gmail.com>
2015-11-06 14:55:36 -0500

    refactored current_email, current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
   end
 
   def signed_in?
-    current_email.present?
+    current_user.signed_in?
   end
 
   def current_email
@@ -19,5 +19,9 @@ class ApplicationController < ActionController::Base
 
   def signed_in_as(email)
     session[:current_email] = email
+  end
+
+  def current_user
+    User.new(session[:current_email])
   end
 end
