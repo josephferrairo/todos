069b150d49015fadac62dedb7e7c6cdd79090921
josephferrairo <josephferrairo@gmail.com>
2015-11-06 14:37:58 -0500

    extract session[:current_email]

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-    session[:current_email] = params[:session][:email]
+    signed_in_as params[:session][:email]
     redirect_to root_path
   end
 end
--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -2,7 +2,7 @@ class TodosController <ApplicationController
   before_filter :authenticate
 
   def index
-    @todos = Todo.where(email: session[:current_email])
+    @todos = Todo.where(email: current_email)
   end
 
   def new
